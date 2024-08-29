b37e2fddaf3ad36a7ab0d5720ef9f980eef978d9
Josh <102478723+realjoshuau@users.noreply.github.com>
2024-08-29 10:38:41 -0700

    spotless

--- src/main/java/frc/robot/subsystems/ampevator/Ampevator.java
+++ src/main/java/frc/robot/subsystems/ampevator/Ampevator.java
@@ -7,48 +7,48 @@
 
 package frc.robot.subsystems.ampevator;
 
-import org.littletonrobotics.junction.Logger;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.utils.DisableSubsystem;
+import org.littletonrobotics.junction.Logger;
 
 public class Ampevator extends DisableSubsystem {
-    private final AmpevatorIO ampevatorIO;
-    private final AmpevatorIOInputsAutoLogged ampevatorIOAutoLogged = new AmpevatorIOInputsAutoLogged();
+  private final AmpevatorIO ampevatorIO;
+  private final AmpevatorIOInputsAutoLogged ampevatorIOAutoLogged =
+      new AmpevatorIOInputsAutoLogged();
 
-    public Ampevator(boolean disabled, AmpevatorIO ampevatorIO) {
-        super(disabled);
-        this.ampevatorIO = ampevatorIO;
-    }
+  public Ampevator(boolean disabled, AmpevatorIO ampevatorIO) {
+    super(disabled);
+    this.ampevatorIO = ampevatorIO;
+  }
 
-    @Override
-    public void periodic() {
-        super.periodic();
-        ampevatorIO.updateInputs(ampevatorIOAutoLogged);
-        Logger.processInputs(this.getClass().getSimpleName(), ampevatorIOAutoLogged);
-    }
+  @Override
+  public void periodic() {
+    super.periodic();
+    ampevatorIO.updateInputs(ampevatorIOAutoLogged);
+    Logger.processInputs(this.getClass().getSimpleName(), ampevatorIOAutoLogged);
+  }
 
-    public Command setPosition(double position) {
-        return this.run(() -> ampevatorIO.setPosition(position));
-    }
+  public Command setPosition(double position) {
+    return this.run(() -> ampevatorIO.setPosition(position));
+  }
 
-    public Command setVoltage(double voltage) {
-        return this.run(() -> ampevatorIO.setVoltage(voltage)).finallyDo(ampevatorIO::off);
-    }
+  public Command setVoltage(double voltage) {
+    return this.run(() -> ampevatorIO.setVoltage(voltage)).finallyDo(ampevatorIO::off);
+  }
 
-    public Command zero() {
-        return this.run(ampevatorIO::zero);
-    }
+  public Command zero() {
+    return this.run(ampevatorIO::zero);
+  }
 
-    public Command off() {
-        return this.run(ampevatorIO::off);
-    }
+  public Command off() {
+    return this.run(ampevatorIO::off);
+  }
 
-    public Command extendAmpevator() {
-        return this.run(() -> ampevatorIO.setPosition(AmpevatorConstants.kAmpevatorUpPosition));
-    }
+  public Command extendAmpevator() {
+    return this.run(() -> ampevatorIO.setPosition(AmpevatorConstants.kAmpevatorUpPosition));
+  }
 
-    public Command retractAmpevator() {
-        return this.run(() -> ampevatorIO.setPosition(AmpevatorConstants.kAmpevatorDownPosition));
-    }
+  public Command retractAmpevator() {
+    return this.run(() -> ampevatorIO.setPosition(AmpevatorConstants.kAmpevatorDownPosition));
+  }
 }
