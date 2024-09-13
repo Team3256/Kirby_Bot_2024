2fc8464290e0d65f1b59eed532863e7c77da188b
isabella354 <87916997+isabella354@users.noreply.github.com>
2024-09-13 15:09:54 -0700

    Apply spotless
    yay it worked

--- src/main/java/frc/robot/subsystems/spindex/Spindex.java
+++ src/main/java/frc/robot/subsystems/spindex/Spindex.java
@@ -1,10 +1,3 @@
-package frc.robot.subsystems.spindex;
-import com.ctre.phoenix6.hardware.TalonFX;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj2.command.Command;
-
-    
 // Copyright (c) 2024 FRC 3256
 // https://github.com/Team3256
 //
@@ -12,30 +5,45 @@ import edu.wpi.first.wpilibj2.command.Command;
 // license that can be found in the LICENSE file at
 // the root directory of this project.
 
-public class Spindex extends SubsystemBase {
-    private TalonFX newMotor = new TalonFX(SpindexConstants.spindexMotorID);
-    private DigitalInput beamBreak = new DigitalInput(SpindexConstants.spindexMotorID)
-    // private beambreak Beambreak = new beamreak(1)
-    public Spindex() {}
+package frc.robot.subsystems.spindex;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+// Copyright (c) 2024 FRC 3256
+// https://github.com/Team3256
+//
+// Use of this source code is governed by a
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+public class Spindex extends SubsystemBase {
+  private TalonFX newMotor = new TalonFX(SpindexConstants.spindexMotorID);
+  private DigitalInput beamBreak = new DigitalInput(SpindexConstants.spindexMotorID);
+
+  // private beambreak Beambreak = new beamreak(1)
+  public Spindex() {}
 
   /**
    * Example command factory method.
    *
    * @return a command
    */
-  public Command spin () {
+  public Command spin() {
     // Inline construction of command goes here.
     // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return run(
-       () -> { 
+    return run(() -> {
           newMotor.set(SpindexConstants.spindexMotorSpeed);
-        }).finallyDo(this::stop).until(beamBreak::get);
+        })
+        .finallyDo(this::stop)
+        .until(beamBreak::get);
   }
+
   public void stop() {
     newMotor.set(0);
   }
-
 
   /**
    * An example method querying a boolean state of the subsystem (for example, a digital sensor).
@@ -51,8 +59,6 @@ public class Spindex extends SubsystemBase {
   public void periodic() {
     // This method will be called once per scheduler run
     super.periodic();
-
-
   }
 
   @Override
--- src/main/java/frc/robot/subsystems/spindex/SpindexConstants.java
+++ src/main/java/frc/robot/subsystems/spindex/SpindexConstants.java
@@ -1,11 +1,14 @@
-import com.ctre.phoenix6.controls.StaticBrake;
+// Copyright (c) 2024 FRC 3256
+// https://github.com/Team3256
+//
+// Use of this source code is governed by a 
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+package frc.robot.subsystems.spindex;
 
 public class SpindexConstants {
-    public static final int spindexMotorID = 0;
-    public static final double spindexMotorSpeed = 0.8;
-    public static final int beambreakID = 1;
-
-
-
-
+  public static final int spindexMotorID = 0;
+  public static final double spindexMotorSpeed = 0.8;
+  public static final int beambreakID = 1;
 }
