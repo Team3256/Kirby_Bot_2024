f9fb299da7b0a98f96bc167f228b609c46be0b00
dmistry26 <mistrydevshree26@gmail.com>
2024-09-22 12:09:27 -0700

    Feat: Spindex(er) (#3)
    * Created spindexer files
    
    * finish the spindex code
    
    * Apply spotless
    
    yay it worked
    
    * change to beambreakID
    
    Co-Authored-By: dmistry26 <172151088+dmistry26@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: isabella354 <87916997+isabella354@users.noreply.github.com>
    Co-authored-by: dmistry26 <172151088+dmistry26@users.noreply.github.com>

---
+++ src/main/java/frc/robot/subsystems/spindex/Spindex.java
@@ -0,0 +1,68 @@
+// Copyright (c) 2024 FRC 3256
+// https://github.com/Team3256
+//
+// Use of this source code is governed by a 
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+package frc.robot.subsystems.spindex;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+// Copyright (c) 2024 FRC 3256
+// https://github.com/Team3256
+//
+// Use of this source code is governed by a
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+public class Spindex extends SubsystemBase {
+  private TalonFX newMotor = new TalonFX(SpindexConstants.spindexMotorID);
+  private DigitalInput beamBreak = new DigitalInput(SpindexConstants.beambreakID);
+
+  // private beambreak Beambreak = new beamreak(1)
+  public Spindex() {}
+
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public Command spin() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return run(() -> {
+          newMotor.set(SpindexConstants.spindexMotorSpeed);
+        })
+        .finallyDo(this::stop)
+        .until(beamBreak::get);
+  }
+
+  public void stop() {
+    newMotor.set(0);
+  }
+
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    super.periodic();
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/spindex/SpindexConstants.java
@@ -0,0 +1,14 @@
+// Copyright (c) 2024 FRC 3256
+// https://github.com/Team3256
+//
+// Use of this source code is governed by a 
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+package frc.robot.subsystems.spindex;
+
+public class SpindexConstants {
+  public static final int spindexMotorID = 0;
+  public static final double spindexMotorSpeed = 0.8;
+  public static final int beambreakID = 1;
+}
